4a1a0ac3a2f3c863133e4e13f2044a6703cf07b4
Billy Zheng <billyzheng.bz@gmail.com>
2024-10-17 12:22:26 -0400

    jaxify integrators

--- f1tenth_gym_jax/envs/integrator.py
+++ f1tenth_gym_jax/envs/integrator.py
@@ -1,160 +1,137 @@
-from abc import abstractmethod
-from enum import Enum
+# other
+from typing import Callable
+from functools import partial
+
+# jax
+import jax
+import chex
+
+# local
+from .f110_env import Param
 
 
-class IntegratorType(Enum):
-    RK4 = 1
-    Euler = 2
+@partial(jax.jit, static_argnums=[3, 4])
+def integrate_rk4(
+    f: Callable, x: chex.Array, u: chex.Array, dt: float, params: Param
+) -> chex.Array:
+    k1 = f(
+        x,
+        u,
+        params.mu,
+        params.C_Sf,
+        params.C_Sr,
+        params.lf,
+        params.lr,
+        params.h,
+        params.m,
+        params.I,
+        params.s_min,
+        params.s_max,
+        params.sv_min,
+        params.sv_max,
+        params.v_switch,
+        params.a_max,
+        params.v_min,
+        params.v_max,
+    )
 
-    @staticmethod
-    def from_string(integrator: str):
-        if integrator == "rk4":
-            return RK4Integrator()
-        elif integrator == "euler":
-            return EulerIntegrator()
-        else:
-            raise ValueError(f"Unknown integrator type {integrator}")
+    k2_state = x + dt * (k1 / 2)
 
+    k2 = f(
+        k2_state,
+        u,
+        params.mu,
+        params.C_Sf,
+        params.C_Sr,
+        params.lf,
+        params.lr,
+        params.h,
+        params.m,
+        params.I,
+        params.s_min,
+        params.s_max,
+        params.sv_min,
+        params.sv_max,
+        params.v_switch,
+        params.a_max,
+        params.v_min,
+        params.v_max,
+    )
 
-class Integrator:
-    def __init__(self) -> None:
-        self._integrator_type = None
+    k3_state = x + dt * (k2 / 2)
 
-    @abstractmethod
-    def integrate(self, f, x, u, dt, params):
-        raise NotImplementedError("integrate method not implemented")
+    k3 = f(
+        k3_state,
+        u,
+        params.mu,
+        params.C_Sf,
+        params.C_Sr,
+        params.lf,
+        params.lr,
+        params.h,
+        params.m,
+        params.I,
+        params.s_min,
+        params.s_max,
+        params.sv_min,
+        params.sv_max,
+        params.v_switch,
+        params.a_max,
+        params.v_min,
+        params.v_max,
+    )
 
-    @property
-    def type(self) -> str:
-        return self._integrator_type
+    k4_state = x + dt * k3
 
+    k4 = f(
+        k4_state,
+        u,
+        params.mu,
+        params.C_Sf,
+        params.C_Sr,
+        params.lf,
+        params.lr,
+        params.h,
+        params.m,
+        params.I,
+        params.s_min,
+        params.s_max,
+        params.sv_min,
+        params.sv_max,
+        params.v_switch,
+        params.a_max,
+        params.v_min,
+        params.v_max,
+    )
 
-class RK4Integrator(Integrator):
-    def __init__(self) -> None:
-        super().__init__()
-        self._integrator_type = "rk4"
-
-    def integrate(self, f, x, u, dt, params):
-        k1 = f(
-            x,
-            u,
-            params["mu"],
-            params["C_Sf"],
-            params["C_Sr"],
-            params["lf"],
-            params["lr"],
-            params["h"],
-            params["m"],
-            params["I"],
-            params["s_min"],
-            params["s_max"],
-            params["sv_min"],
-            params["sv_max"],
-            params["v_switch"],
-            params["a_max"],
-            params["v_min"],
-            params["v_max"],
-        )
-
-        k2_state = x + dt * (k1 / 2)
-
-        k2 = f(
-            k2_state,
-            u,
-            params["mu"],
-            params["C_Sf"],
-            params["C_Sr"],
-            params["lf"],
-            params["lr"],
-            params["h"],
-            params["m"],
-            params["I"],
-            params["s_min"],
-            params["s_max"],
-            params["sv_min"],
-            params["sv_max"],
-            params["v_switch"],
-            params["a_max"],
-            params["v_min"],
-            params["v_max"],
-        )
-
-        k3_state = x + dt * (k2 / 2)
-
-        k3 = f(
-            k3_state,
-            u,
-            params["mu"],
-            params["C_Sf"],
-            params["C_Sr"],
-            params["lf"],
-            params["lr"],
-            params["h"],
-            params["m"],
-            params["I"],
-            params["s_min"],
-            params["s_max"],
-            params["sv_min"],
-            params["sv_max"],
-            params["v_switch"],
-            params["a_max"],
-            params["v_min"],
-            params["v_max"],
-        )
-
-        k4_state = x + dt * k3
-
-        k4 = f(
-            k4_state,
-            u,
-            params["mu"],
-            params["C_Sf"],
-            params["C_Sr"],
-            params["lf"],
-            params["lr"],
-            params["h"],
-            params["m"],
-            params["I"],
-            params["s_min"],
-            params["s_max"],
-            params["sv_min"],
-            params["sv_max"],
-            params["v_switch"],
-            params["a_max"],
-            params["v_min"],
-            params["v_max"],
-        )
-
-        # dynamics integration
-        x = x + dt * (1 / 6) * (k1 + 2 * k2 + 2 * k3 + k4)
-        return x
+    # dynamics integration
+    x = x + dt * (1 / 6) * (k1 + 2 * k2 + 2 * k3 + k4)
+    return x
 
 
-class EulerIntegrator(Integrator):
-    def __init__(self) -> None:
-        super().__init__()
-        self._integrator_type = "euler"
-
-    def integrate(self, f, x, u, dt, params):
-        dstate = f(
-            x,
-            u,
-            params["mu"],
-            params["C_Sf"],
-            params["C_Sr"],
-            params["lf"],
-            params["lr"],
-            params["h"],
-            params["m"],
-            params["I"],
-            params["s_min"],
-            params["s_max"],
-            params["sv_min"],
-            params["sv_max"],
-            params["v_switch"],
-            params["a_max"],
-            params["v_min"],
-            params["v_max"],
-        )
-        x = x + dt * dstate
-        return x
+@partial(jax.jit, static_argnums=[3, 4])
+def integrate_euler(
+    f: Callable, x: chex.Array, u: chex.Array, dt: float, params: Param
+) -> chex.Array:
+    dstate = f(
+        x,
+        u,
+        params.mu,
+        params.C_Sf,
+        params.C_Sr,
+        params.lf,
+        params.lr,
+        params.h,
+        params.m,
+        params.I,
+        params.s_min,
+        params.s_max,
+        params.sv_min,
+        params.sv_max,
+        params.v_switch,
+        params.a_max,
+        params.v_min,
+        params.v_max,
+    )
+    x = x + dt * dstate
+    return x

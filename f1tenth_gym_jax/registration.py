9ad13e76ec47860268ed4d14e8ff99f2768887d2
Hongrui Zheng <billyzheng.bz@gmail.com>
2024-11-04 22:04:14 -0500

    adding scenario parser for make()

--- f1tenth_gym_jax/registration.py
+++ f1tenth_gym_jax/registration.py
@@ -1,7 +1,22 @@
 from .envs import F110Env
 
+# scenario patterns
+# {num_agent}_{produce_scan}_{reward_type}_v0
+# {int}_{"scan"/"noscan"}_{"time+/progress+/"}_v0
+def _parse_scenario(scenario: str):
+    scenario = scenario.split("_")
+    num_agents = int(scenario[0])
+    # check whether num_agents is valid
+    if num_agents < 1:
+        raise ValueError(f"Invalid number of agents: {num_agents}")
+    produce_scan = scenario[1] == "scan"
+    reward_type = scenario[2]
+    all_reward_function = reward_type.split("+")
+    assert reward_type in ["time", ""], f"Invalid reward type: {reward_type}, must be one of ['time', '']"
+    return num_agents, produce_scan, reward_type
+
+
 def make(env_id: str, **env_kwargs):
-    """A JAX-version of OpenAI's env.make(env_name), built off Gymnax"""
     if env_id not in registered_envs:
         raise ValueError(f"{env_id} is not a registered environment.")
 

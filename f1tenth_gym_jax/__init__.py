1dc1c73526da825fbb20a89b3c43eb586e2ba047
Billy Zheng <billyzheng.bz@gmail.com>
2025-07-01 10:57:57 -0400

    remove trajrenderer unused import

--- f1tenth_gym_jax/__init__.py
+++ f1tenth_gym_jax/__init__.py
@@ -1,5 +1,4 @@
 from .registration import make
-from .envs import TrajRenderer
 
 __all__ = ["make"]
 __version__ = "0.0.1"
--- f1tenth_gym_jax/envs/__init__.py
+++ f1tenth_gym_jax/envs/__init__.py
@@ -1,2 +1,1 @@
 from .f110_env import F110Env  # noqa: F401 This is needed to make easy imports work
-from .rendering import TrajRenderer  # noqa: F401 This is needed to make easy imports work
